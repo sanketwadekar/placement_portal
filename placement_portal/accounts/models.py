43f28677c90f05bebd801ec11848c1a4a5b58bcc
sanketvarpe <varpesanket2000@gmail.com>
2020-11-22 19:13:39 +0530

    company and job model added

--- placement_portal/accounts/models.py
+++ placement_portal/accounts/models.py
@@ -3,29 +3,28 @@ from django.contrib.auth.models import User
 # Create your models here.
 #company model
 
-# class companyModel(models.Model):
+class companyModel(models.Model):
 
-#   #cId = models.IntegerField(primary_key = True,auto_created = True)
-#   name = models.CharField(max_length = 100, blank = False, unique = True)
-#   address = models.CharField(max_length = 100, blank = False)
-#   contact = models.IntegerField(blank = False, unique = True)
-#   about = models.CharField(max_length = 50, blank = True)
+  #cId = models.IntegerField(primary_key = True,auto_created = True)
+  name = models.CharField(max_length = 100, blank = False, unique = True)
+  address = models.CharField(max_length = 100, blank = False)
+  contact = models.CharField(max_length = 12,blank = False, unique = True)
+  about = models.CharField(max_length = 50, blank = True)
 
-#   def __str__(self):
-# 	  return self.name
+  def __str__(self):
+	  return self.name
 # # #job model
 # def path_to_upload_jd(instance,filename):
 #   return 'JD/jd_{0}'.format(instance.cName)
-# class jobModel(models.Model):
+class jobModel(models.Model):
 
-#   #jobId = models.IntegerField(primary_key = True,unique = True,auto_created = True)
-#   cName = models.ForeignKey(companyModel,on_delete= models.SET_NULL, null = True)
-#   salary = models.FloatField(blank = True)
-#   dateOfArrival = models.DateField()
-#   lastDateToApply = models.DateField()
-#   jdFile = models.FileField(upload_to = path_to_upload_jd)
+  #jobId = models.IntegerField(primary_key = True,unique = True,auto_created = True)
+  cName = models.ForeignKey(companyModel,on_delete= models.SET_NULL, null = True)
+  salary = models.CharField(max_length = 20,blank = True)
+  dateOfArrival = models.DateField()
+  lastDateToApply = models.DateField()
+  jdFile = models.FileField(upload_to = 'JD/')
+  
+#   def __str__(self):
+# 	  return self.cName
 
-#jdTemplate model
-
-# class applicantModel(models.Model):
-# #   jdLink = models
--- placement_portal/placement_portal/urls.py
+++ placement_portal/placement_portal/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('accounts.urls'))
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Placement Portal Administration'
+admin.site.index_title = 'Welcome to Placement-Portal'
+admin.site.site_title = 'Placement-Portal'
