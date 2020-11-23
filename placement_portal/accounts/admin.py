2391de9e8a45aed21e3d4a4d7fd76f9044fd6ca5
sanketvarpe <varpesanket2000@gmail.com>
2020-11-23 13:57:45 +0530

    added applicant and notice model

--- placement_portal/accounts/admin.py
+++ placement_portal/accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from  .models import *
 # Register your models here.
+
+class applicantAdmin(admin.ModelAdmin):
+    list_display = ('user','jobId','status')
+    #list_per_page = 10
+    list_filter = ('status','jobId',)
+
 admin.site.register(companyModel)
 admin.site.register(jobModel)
+admin.site.register(noticeModel)
+admin.site.register(applicantModel,applicantAdmin)
