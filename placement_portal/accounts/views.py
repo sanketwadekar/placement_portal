7585633c81182cb0d531f77ac650102650c51f47
Sanket Wadekar <67091512+sanketwadekar@users.noreply.github.com>
2020-11-25 15:36:00 +0530

    Added paging to dashboard template

--- placement_portal/accounts/views.py
+++ placement_portal/accounts/views.py
@@ -47,10 +47,10 @@ def loginView(request):
 def homeView(request):
     jobs = jobModel.objects.filter().order_by('-lastDateToApply')
     jobPaginator = Paginator(jobs,6)
-    page_number = request.GET.get('page_number')
+    page_number = request.GET.get('page')
     page_obj = jobPaginator.get_page(page_number)
     context = {'jobs':jobs,'page_obj':page_obj}
-    return render(request, 'accounts/dashboard.html', )
+    return render(request, 'accounts/dashboard.html', context)
 
 def logoutView(request):
     logout(request)
@@ -59,8 +59,6 @@ def logoutView(request):
 @login_required(login_url='login')
 def noticeView(request):
     notices = noticeModel.objects.filter().order_by('-date')
-    print(notices[0].date)
-    print(notices[1].date)
     noticePaginator = Paginator(notices,6)
     page_number = request.GET.get('page_number')
     page_obj = noticePaginator.get_page(page_number)
@@ -71,27 +69,23 @@ def noticeView(request):
 def applicantView(request):
     print(request.user)
     application = applicantModel.objects.filter(user = request.user)
-    #print(application[0].status)
-    #print(application[0].jobId)
-    #print(application[0].user)
     if not application:
         print("no application found")
         return HttpResponse("There are no applications")
-    #print(application.jobId_id)
     context = {'applicant' : application}
     return render(request, 'accounts/dashboard.html', context)
 
-#apply to job view
+
 @login_required(login_url='login')
 def applyToJobView(request,jobId):
     condition1 = Q(user = request.user)
     condition2 = Q(jobId = jobId)
     checkIfApplied = applicantModel.objects.filter(condition1 & condition2)
-
     if len(checkIfApplied):
         messages.info(request,'Already applied...')
     else:
-        applicant = applicantModel(user = request.user, jobId = jobId, status = 'In Process')
+        applicant = applicantModel(user = request.user, jobId = jobModel(id = jobId), status = 'In Process')
+        applicant.save()
         messages.success(request,'Applied successfully...')
     
     return redirect('home')
