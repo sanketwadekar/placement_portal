14cf9cbc5ad5000ee2a72d8c5b7b02b016a9f770
sanketvarpe <varpesanket2000@gmail.com>
2020-11-27 14:23:26 +0530

    file genration added

--- placement_portal/customAdmin/urls.py
+++ placement_portal/customAdmin/urls.py
@@ -5,5 +5,6 @@ app_name = 'customAdmin'
 
 urlpatterns = [
     path('jobs/',views.adminJobView,name = 'customAdminJob'),
-    path('view/applications/<str:jobId>',views.adminJobApplicationsView,name = 'customAdminApplications')
+    path('view/applications/<str:jobId>',views.adminJobApplicationsView,name = 'customAdminApplications'),
+    path('view/applications/download/<str:jobId>',views.adminDownloadCsvFile,name = 'customAdminDownload')
 ]
