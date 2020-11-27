ce6895555ecf781affbd0bd5497788229e6eb02c
sanketvarpe <varpesanket2000@gmail.com>
2020-11-27 10:21:58 +0530

    mailEngine modified

--- placement_portal/accounts/mailEngine.py
+++ placement_portal/accounts/mailEngine.py
@@ -1,25 +1,23 @@
 from django.core.mail import send_mail 
 from django.core.mail import EmailMultiAlternatives 
-from django.template.loader import get_template 
+from django.template.loader import get_template,render_to_string 
 from django.template import Context 
 from django.conf import settings
 from django.shortcuts import render
 
 def sendRegistrationMail(request,subject,htmlTemplateName,receipents = []):
     subject, from_email, to = subject, settings.EMAIL_HOST_USER, receipents
-    htmlTemplate = get_template('templates/accounts/registerMailTemplate.html')
+    htmlTemplate = get_template('accounts/registerMailTemplate.html')
     context = {}
     #html template
 
-    html_content = htmlTemplate.render(context)
-    msg = EmailMultiAlternatives(subject, html_content, from_email, [to]) 
-    msg.attach_alternative(html_content, "text / html") 
+    html_content = render_to_string('accounts/registerMailTemplate.html',context)
 
     #attach files
     #msg.attach_file(path = 'path/to/file')
     #send mail
 
-    res = msg.send()
+    res = send_mail(subject, "hello",from_email,[to],html_message = html_content)
     if res!=1:
         print("email process failed...")
     else:
